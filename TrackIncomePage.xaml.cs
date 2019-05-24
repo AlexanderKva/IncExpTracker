754538981c9ec525d0656218d16818d977788108
Alexander Kvasniak <akvasniak@masoutis.gr>
2019-05-24 14:52:02 +0300

    Update TrackIncomePage.xaml.cs

--- TrackIncomePage.xaml.cs
+++ TrackIncomePage.xaml.cs
@@ -61,25 +61,16 @@ namespace IncExpTracker
            
             _vmEntryList.IsBusy = true;
             List<IncomeTrack> incomeEntries = new List<IncomeTrack>();
-            if (date.Month == DateTime.Now.Month && date.Year == DateTime.Now.Year)
-            {
-                incomeEntries = SelectedMonthIncome(DateTime.Now.Month, DateTime.Now.Year);
-            }
-            else
-            {
-                incomeEntries = SelectedMonthIncome(date.Month, date.Year);
-            }
-
-            VMEntryList payDayOutput = new VMEntryList();
-            VMEntryList tipsOutput = new VMEntryList();
-            VMEntryList othersOutput = new VMEntryList();
-
+            incomeEntries = SelectedMonthIncome(date.Month, date.Year);
+            
+            VMEntryList listToShow = new VMEntryList();
+            
             // ..Sorting out Categories..
             foreach (var s in incomeEntries)
             {
-                if (s.Descr == "PayDay")
+                if (s.Descr == descr)
                 {
-                    payDayOutput.EntryList.Add(new VMEntry
+                    listToShow.EntryList.Add(new VMEntry
                     {
                         Id = s.Id,
                         Descr = s.Descr,
@@ -87,77 +78,19 @@ namespace IncExpTracker
                         Date = s.Date,
                         DateToString = s.Date.DayOfWeek + " " + s.Date.ToString("dd MMM HH:mm")
                     });
-                    payDayOutput.Summary += s.Amount;
-
+                    listToShow.Summary += s.Amount;
                 }
-                else if (s.Descr == "Tips")
-                {
-                    tipsOutput.EntryList.Add(new VMEntry
-                    {
-                        Id = s.Id,
-                        Descr = s.Descr,
-                        Amount = s.Amount,
-                        Date = s.Date,
-                        DateToString = s.Date.DayOfWeek + " " + s.Date.ToString("dd MMM HH:mm")
-                    });
-                    tipsOutput.Summary += s.Amount;
-                }
-                else
-                {
-                    othersOutput.EntryList.Add(new VMEntry
-                    {
-                        Id = s.Id,
-                        Descr = s.Descr,
-                        Amount = s.Amount,
-                        Date = s.Date,
-                        DateToString = s.Date.DayOfWeek + " " + s.Date.ToString("dd MMM HH:mm")
-                    });
-                    othersOutput.Summary += s.Amount;
-                }
-
             }
+
+            _vmEntryList = listToShow;
             // ..Checks if its the current month..
             if (date.Month == DateTime.Now.Month && date.Year == DateTime.Now.Year)
             {
-                // ..Links selected Category with BindingContext..
-                if (descr == "PayDay")
-                {
-                    _vmEntryList = payDayOutput;
-                    secTitle.Text = "This month you got " + _vmEntryList.Summary + " from PayDay!";
-
-
-                }
-                else if (descr == "Tips")
-                {
-                    _vmEntryList = tipsOutput;
-                    secTitle.Text = "This month you got " + _vmEntryList.Summary + " from Tips!";
-                }
-                else
-                {
-                    _vmEntryList = othersOutput;
-                    secTitle.Text = "This month you got " + _vmEntryList.Summary + " from Other Resources!";
-                }
+                secTitle.Text = "This month you got " + _vmEntryList.Summary + " from "+ descr + "!";
             }
             else
             {
-                // ..Links selected Category with BindingContext..
-                if (descr == "PayDay")
-                {
-                    _vmEntryList = payDayOutput;
-                    secTitle.Text = "On " + DateTimeFormatInfo.CurrentInfo.GetAbbreviatedMonthName(date.Month) + " you got " + _vmEntryList.Summary + " from PayDay!";
-
-
-                }
-                else if (descr == "Tips")
-                {
-                    _vmEntryList = tipsOutput;
-                    secTitle.Text = "On " + DateTimeFormatInfo.CurrentInfo.GetAbbreviatedMonthName(date.Month) + " you got " + _vmEntryList.Summary + " from Tips!";
-                }
-                else
-                {
-                    _vmEntryList = othersOutput;
-                    secTitle.Text = "On " + DateTimeFormatInfo.CurrentInfo.GetAbbreviatedMonthName(date.Month) + " you got " + _vmEntryList.Summary + " from Other Resources!";
-                }
+                secTitle.Text = "On " + DateTimeFormatInfo.CurrentInfo.GetAbbreviatedMonthName(date.Month) + " you got " + _vmEntryList.Summary + " from " + descr + "!";
             }
         }
 
